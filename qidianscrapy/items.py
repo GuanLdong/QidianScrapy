b5f4be8de8aab9d9dc46b5355b8a4a5da5fdb620
GuanLdong <guancarlous@gmail.com>
2019-05-27 18:00:18 +0800

    add commmits

--- qidianscrapy/items.py
+++ qidianscrapy/items.py
@@ -11,11 +11,19 @@ import scrapy
 class QidianscrapyItem(scrapy.Item):
     # define the fields for your item here like:
     collection = table = 'Qidian'
+    # Author of novle
     Author = scrapy.Field()
+    # title of novle
     Title = scrapy.Field()
+    # Request url
     Url = scrapy.Field()
+    # breief instruction
     Content = scrapy.Field()
+    # main class
     FictionClass1 = scrapy.Field()
+    # second class
     FictionClass2 = scrapy.Field()
+    # Serialization status
     State = scrapy.Field()
+    # word number
     Number = scrapy.Field()
